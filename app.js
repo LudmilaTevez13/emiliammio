4bff2f480b89b7a6caf10f08c18bd7e4be3b0308
LudmilaTevez13 <tevez.ludmila13@gmail.com>
2023-07-22 23:14:43 -0300

    feat: agrega estructura header

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const PORT = 3030;
 
 /* rutas */
 app.get('/', (req,res) => res.sendFile(path.join(__dirname,'views','home.html')))
-app.get('/nosotros', (req,res) => res.sendFile(path.join(__dirname,'views','about.html')))
-app.get('/musica', (req,res) => res.sendFile(path.join(__dirname,'views','music.html')))
-app.get('/contacto', (req,res) => res.sendFile(path.join(__dirname,'views','contact.html')))
+app.get('/about', (req,res) => res.sendFile(path.join(__dirname,'views','about.html')))
+app.get('/music', (req,res) => res.sendFile(path.join(__dirname,'views','music.html')))
+app.get('/contact', (req,res) => res.sendFile(path.join(__dirname,'views','contact.html')))
 
 app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT}`))
