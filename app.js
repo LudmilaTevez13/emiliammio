03c05dcefef853c6a54a440b69ea42753546ac7d
LudmilaTevez13 <tevez.ludmila13@gmail.com>
2023-07-23 17:51:59 -0300

    feat : agrega 404

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = 3030;
 
 /* rutas */
-app.get('/', (req,res) => res.sendFile(path.join(__dirname,'views','home.html')))
-app.get('/about', (req,res) => res.sendFile(path.join(__dirname,'views','about.html')))
-app.get('/music', (req,res) => res.sendFile(path.join(__dirname,'views','music.html')))
-app.get('/contact', (req,res) => res.sendFile(path.join(__dirname,'views','contact.html')))
-
-app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT}`))
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "home.html"))
+);
+app.get("/about", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "about.html"))
+);
+app.get("/music", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "music.html"))
+);
+app.get("/contact", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "contact.html"))
+);
+app.get("/*", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "404notfound.html"))
+);
+app.listen(PORT, () =>
+  console.log(`Server running in http://localhost:${PORT}`)
+);
